049cb39057df89ceab75ee82af0e0a22ae6ca191
TheMowgliMan <96443213+TheMowgliMan@users.noreply.github.com>
2022-01-01 15:25:14 -0500

    Add Main Menu

--- Lareata/Player/Player.gd
+++ Lareata/Player/Player.gd
@@ -8,17 +8,26 @@ export var SPEED = 200
 var velocity = Vector2()
 export var JUMPSPEED = 250
 
-func _physics_process(delta):
-	velocity.y += GRAVITY * delta
-	
-	if Input.is_action_pressed("ui_move_left"):
-		velocity.x = -SPEED
-	elif Input.is_action_pressed("ui_move_right"):
-		velocity.x =  SPEED
-	else:
-		velocity.x = 0
-	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
-		velocity.y -= JUMPSPEED
+var playing = false
 
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+func _physics_process(delta):
+	if playing:
+		$Graphics.visible = true
+		
+		velocity.y += GRAVITY * delta
+		
+		if Input.is_action_pressed("ui_move_left"):
+			velocity.x = -SPEED
+		elif Input.is_action_pressed("ui_move_right"):
+			velocity.x =  SPEED
+		else:
+			velocity.x = 0
+		if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+			velocity.y -= JUMPSPEED
+
+		velocity = move_and_slide(velocity, Vector2(0, -1))
+	else:
+		$Graphics.visible = false
 	
+func _on_Mapgen_mapgen_finished():
+	playing = true
--- Lareata/World/Mapgen.gd
+++ Lareata/World/Mapgen.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 var map = []
 
-export var map_width = 320
-export var map_height = 650
+export var map_width = 640
+export var map_height = 640
 export var cave_tolerance = 0.15
+var percentThroughMapgen = 0
+var go = false
+var m
+
+signal mapgen_finished
 
 enum blocks {
 	AIR,
@@ -56,10 +61,14 @@ func mapgen():
 		var change = noiseLand.get_noise_1d(x) * 4
 		land_point += change
 		
-		if (x / map_width * 100) == 25:
-			print("Putting dirt in stone...")
-		if (x / map_width * 100) == 66:
-			print("Placing ore...")
+		percentThroughMapgen = x
+		
+		if x % 5 == 4:
+			yield()
+			#$ProgressBarContainer/ProgressContainer/VBoxContainer/BarMargin/ProgressBar.value = percentThroughMapgen
+#			VisualServer.force_draw()
+			#get_tree().get_root().update_worlds()
+			#propagate_notification(NOTIFICATION_DRAW)
 		
 		for y in range(map_height):
 			var cave_value = noiseLand.get_noise_2d(x, y)
@@ -75,10 +84,21 @@ func mapgen():
 					place_ore(x, y)
 			else:
 				set_map(x, y, blocks.AIR)
+	go = false
+				
+#func _process(delta):
+#	$ProgressBarContainer/ProgressContainer/VBoxContainer/BarMargin/ProgressBar.value = percentThroughMapgen
+#	get_tree().get_root().update_worlds()
+#	if go == true:
+#		m.resume()
+#	print(percentThroughMapgen)
 
 func _ready():
+	$ProgressBarContainer.visible = false
+
+func _on_MainMenu_start_game(world_seed):
 	# Configure
-	noiseLand.seed = randi()
+	noiseLand.seed = world_seed
 	noiseLand.octaves = 2
 	noiseLand.period = 20.0
 	noiseLand.persistence = 0.8
@@ -88,8 +108,22 @@ func _ready():
 	noiseOre.period = 7.0
 	noiseOre.persistence = 0.8
 	
-	mapgen()
+	$ProgressBarContainer.visible = true
+	$ProgressBarContainer/ProgressContainer/VBoxContainer/BarMargin/ProgressBar.max_value = map_width
+	
+	yield(get_tree().create_timer(0.1), "timeout")
+	m = mapgen()
+	go = true
+	
+	while go == true:
+		$ProgressBarContainer/ProgressContainer/VBoxContainer/BarMargin/ProgressBar.value = percentThroughMapgen
+		yield(get_tree().create_timer(0.03), "timeout")
+		print(percentThroughMapgen)
+		m = m.resume()
 	
 	for x in range(map_width):
 		for y in range(map_height):
 			$Map.set_cell(x, y, get_map(x, y))
+			
+	$ProgressBarContainer.visible = false
+	emit_signal("mapgen_finished")
