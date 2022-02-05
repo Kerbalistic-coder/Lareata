c4904df0bcd46e6156759512c8bc2f1b239673e1
TheMowgliMan <96443213+TheMowgliMan@users.noreply.github.com>
2022-02-05 15:20:11 -0500

    Remove Old Lareata Copy

--- Lareata/World/World.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(delta):
-	var pos = $Player.position
-	
-	$Mapgen.player_x = pos.x
-	$Mapgen.player_y = pos.y
-	
-	$Panel.rect_position = $Player.position - Vector2(400, 225)
